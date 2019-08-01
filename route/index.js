096d1b29405f221c446d338342c74b3429e189c2
Kyle Clabough <kaclabough@gmail.com>
2019-08-01 15:26:19 -0400

    completed route/index.js

---
+++ route/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/", require("./application"));
+router.use("/api/digimon", require("./digimon"));
+router.use("./api/moves", require("./moves"));
+router.use("./api/skills", require("./skills"));
+
+module.exports = router;
