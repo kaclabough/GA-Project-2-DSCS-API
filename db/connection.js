c7fe057941c0520e3ce6fa82dc742afb40f89585
Kyle Clabough <kaclabough@gmail.com>
2019-08-01 09:33:42 -0400

    connection.js

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+
+const mongoURI = "mongodb://localhost/digmoncs-api";
+
+mongoose.connect(mongoURI, {useNewUrlParser: true})
+    .then(instance => 
+        console.log(`connected to db: ${instance,connections[0].name}`)
+    ).catch(err => console.log("Connection Failed.", err));
+
+module.exports = mongoose;
