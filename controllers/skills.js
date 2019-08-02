e5ec630f19ae8c0635ecc64f487c1d06c2533e9a
Kyle Clabough <kaclabough@gmail.com>
2019-08-01 23:57:10 -0400

    skill controllers complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,5 +5,34 @@ module.exports = {
     Skill.find({}).then(skills => {
       res.json(skills);
     });
+  },
+  showId: (req, res) => {
+    Skill.findOne({ _id: req.params.id }).then(skills => {
+      res.json(skills);
+    });
+  },
+  showName: (req, res) => {
+    Skill.findOne({ name: req.params.name.toLowerCase() }).then(skill => {
+      res.json(skill);
+    });
+  },
+  create: (req, res) => {
+    Skill.create(req.body).then(skill => res.json(skill));
+  },
+  update: (req, res) => {
+    let search = req.params.name.toLowerCase();
+    let firstLetter = search[0].toUpperCase();
+    search = firstLetter + search.slice(1);
+    Skill.findOneAndUpdate({ name: search }, req.body).then(skill => {
+      res.json(skill);
+    });
+  },
+  delete: (req, res) => {
+    let search = req.params.name.toLowerCase();
+    let firstLetter = search[0].toUpperCase();
+    search = firstLetter + search.slice(1);
+    Skill.findOneAndDelete({ name: search }).then(skill => {
+      res.json(skill);
+    });
   }
 };
--- route/skills.js
+++ route/skills.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const skillController = require("../controllers/skills");
 
 router.get("/", skillController.index);
-
+router.get("/id/:id", skillController.showId);
+router.get("/name/:name", skillController.showName);
+router.post("/", skillController.create);
+router.put("/:name", skillController.update);
+router.delete("/:name", skillController.delete);
 module.exports = router;
